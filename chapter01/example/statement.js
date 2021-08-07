b1a2921d2285476badbb26a2b047b941119a1330
Namju <skawn11114@gmail.com>
2021-08-08 01:43:53 +0900

    refactoring : perf -> performance _ 명확한 이름으로 변경

--- chapter01/example/statement.js
+++ chapter01/example/statement.js
@@ -22,21 +22,21 @@ function statement(invoice, plays) {
   return result;
 }
 
-function amountFor(perf, play) {
+function amountFor(performance, play) {
   let result = 0;
   switch (play.type) {
     case 'tragedy':
       result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (performance.audience > 30) {
+        result += 1000 * (performance.audience - 30);
       }
       break;
     case 'comedy':
       result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (performance.audience > 20) {
+        result += 10000 + 500 * (performance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * performance.audience;
       break;
     default:
       throw new Error(`알 수 없는 장르: ${play.type}`);
