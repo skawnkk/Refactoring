02dcf3d4caf7d29366d82c831b6ce60aa2f17b3f
Namju <skawn11114@gmail.com>
2021-08-08 02:57:59 +0900

    refactoring : 매개변수 totalAmount 함수화 및 인라인 적용

--- chapter01/example/statement.js
+++ chapter01/example/statement.js
@@ -2,16 +2,21 @@ import { plays } from './plays.js';
 import { invoice } from './invoice.js';
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
-
   for (let perf of invoice.performances) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))}  (${perf.audience}석)\n`;
-    totalAmount += amountFor(perf);
   }
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
+}
+
+function totalAmount() {
+  let totalAmount = 0;
+  for (let perf of invoice.performances) {
+    totalAmount += amountFor(perf);
+  }
+  return totalAmount;
 }
 
 function totalVolumeCredits() {
