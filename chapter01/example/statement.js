4062b783df09890aa93abedcd6cab6055411da17
Namju <skawn11114@gmail.com>
2021-08-08 02:54:50 +0900

    refactoring : 매개변수 volumeCredits를 함수화 및 인라인
    -반복문을 쪼갠 후에 함수화시키고 함수를 인라인으로 적용해 변수를 제거함

--- chapter01/example/statement.js
+++ chapter01/example/statement.js
@@ -3,17 +3,23 @@ import { invoice } from './invoice.js';
 
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0; //포인트(다음 공연 의뢰시 할인)
   let result = `청구내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumCreditsFor(perf);
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))}  (${perf.audience}석)\n`;
     totalAmount += amountFor(perf);
   }
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${volumeCredits}점\n`;
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
+}
+
+function totalVolumeCredits() {
+  let volumeCredits = 0; //포인트(다음 공연 의뢰시 할인)
+  for (let perf of invoice.performances) {
+    volumeCredits += volumCreditsFor(perf);
+  }
+  return volumeCredits;
 }
 
 function usd(number) {
